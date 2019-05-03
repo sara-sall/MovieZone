55be256ad2e120224d1dac7e504ab1477424d4ac
TomHomeComputer <Tom.gustavsson@live.se>
2019-05-03 16:48:17 +0200

    Design update

--- app/src/main/java/com/example/moviezone/MainActivity.java
+++ app/src/main/java/com/example/moviezone/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.moviezone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
 
         //Toms kommentar
         //joakim
@@ -19,10 +27,18 @@ public class MainActivity extends AppCompatActivity {
 
         // VÅRAN API NYCKEL: 844f26c4
 
-        Movie movie = new Movie("Shawshank redemption", 1994, "Drama", 9.4,
+       /* Movie movie = new Movie("Shawshank redemption", 1994, "Drama", 9.4,
                 "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtOD" +
                         "M1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg");
 
-        Log.d("!!!", movie.getTitle());
+        Log.d("!!!", movie.getTitle()); */
+
+       startButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this, QuestionsActivity.class);
+               startActivity(intent);
+           }
+       });
     }
 }
