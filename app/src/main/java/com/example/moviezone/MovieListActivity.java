f8448092e303fd22030b2ee857550e5e890338d2
sara-sall <sarasall092@gmail.com>
2019-05-15 15:31:02 +0200

    Created activities and classes for MovieList

---
+++ app/src/main/java/com/example/moviezone/MovieListActivity.java
@@ -0,0 +1,203 @@
+package com.example.moviezone;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+
+
+    }
+
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        private ArrayList <Movie> movieList = new ArrayList<>();
+        private RequestQueue volleyQueue;
+
+        private RecyclerView recyclerView;
+        private RecyclerView.LayoutManager layoutManager;
+        private MovieListAdapter movieAdapter;
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_movie_list, container, false);
+            volleyQueue = Volley.newRequestQueue(getContext());
+            recyclerView = rootView.findViewById(R.id.recyclerID);
+            layoutManager = new LinearLayoutManager(getActivity());
+            recyclerView.setLayoutManager(layoutManager);
+
+            movieAdapter = new MovieListAdapter(movieList);
+            recyclerView.setAdapter(movieAdapter);
+            String query = "";
+
+            if(getArguments().getInt(ARG_SECTION_NUMBER) == 1){
+
+                query = "https://api.themoviedb.org/3/discover/movie?api_key=d0532d41c9054bf65a4ec278b98fd6cf&language=en-US&" +
+                        "sort_by=popularity.desc&include_adult=false&include_video=false&page=1&";
+
+            }
+            else if(getArguments().getInt(ARG_SECTION_NUMBER) == 2){
+                query = "https://api.themoviedb.org/3/discover/movie?api_key=d0532d41c9054bf65a4ec278b98fd6cf&language=en-US&" +
+                        "sort_by=popularity.asc&include_adult=false&include_video=false&page=1&";
+
+            }
+
+            jsonParse(query);
+
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+
+        private void jsonParse(String url){
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                JSONArray jsonArray = response.getJSONArray("results");
+
+                                for (int i = 0; i < jsonArray.length(); i++){
+                                    JSONObject film = jsonArray.getJSONObject(i);
+
+
+                                    movieList.add(new Movie(film.getString("title"), film.getString("release_date"), film.getDouble("vote_average"),
+                                            film.getString("poster_path")));
+
+                                    Log.d("###", movieList.toString());
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+
+            volleyQueue.add(request);
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/moviezone/MovieListAdapter.java
@@ -0,0 +1,77 @@
+package com.example.moviezone;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MovieListAdapter extends RecyclerView.Adapter{
+    private static List<Movie> movieList;
+    private Context context;
+    private TextView movieTitle;
+    private TextView movieRating;
+    private ImageView movieImage;
+
+
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private TextView movieTitle;
+        private TextView movieRating;
+        private ImageView movieImage;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+            movieImage = itemView.findViewById(R.id.movieListImage);
+            movieRating = itemView.findViewById(R.id.movieListRating);
+            movieTitle = itemView.findViewById(R.id.movieListTitle);
+
+
+
+        }
+
+        @Override
+        public void onClick(View v) {
+           int position = getAdapterPosition();
+           final Movie movie = movieList.get(position);
+
+           if(v.getId() == R.id.movieListItem){
+
+           }
+
+        }
+
+    }
+
+    public MovieListAdapter(List <Movie> movieList) {
+        this.movieList = movieList;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_movie_list, viewGroup, false);
+        MovieViewHolder movieViewHolder = new MovieViewHolder(view);
+        context = viewGroup.getContext();
+        return movieViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder vh, int i) {
+        vh.movieTitle.setText(movieList.get(i).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+}
